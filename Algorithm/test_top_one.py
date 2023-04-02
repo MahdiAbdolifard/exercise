4dba6a9beb6cfa0a0277e9d7e868da37679a59c4
M8hdi <abdolifardmahdi@gmail.com>
2023-04-02 12:36:23 +0330

    unittest

--- Algorithm/test_top_one.py
+++ Algorithm/test_top_one.py
@@ -5,7 +5,7 @@ class TestTop_one(unittest.TestCase):
     def tset_top_one(self):
         self.assertEqual(top_one.top_one([1, 2, 3, 2, 2, 3, 4, 5, 5, 5, 2, 2]), 
                         "The number 2 is the most repeated (5 times).")
-        
+
 
 if __name__ == '__main__' :
     unittest.main()
